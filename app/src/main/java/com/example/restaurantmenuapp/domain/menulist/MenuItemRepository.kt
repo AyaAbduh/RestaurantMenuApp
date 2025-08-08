788a691f71617da7ed5d39128601468cca79c6d8
Aya Abdo <aya.yoya962015@gmail.com>
2025-08-08 16:40:24 +0200

    deleting unneeded logs

--- app/src/main/java/com/example/restaurantmenuapp/domain/menulist/MenuItemRepository.kt
+++ app/src/main/java/com/example/restaurantmenuapp/domain/menulist/MenuItemRepository.kt
@@ -18,16 +18,13 @@ class MenuItemRepository@Inject constructor(
     suspend fun getMenuItems(): List<MenuItem> {
         return if (NetworkUtil.isConnected) {
             try {
-                Log.d("MenuItemRepositoryyyyy",NetworkUtil.isConnected.toString())
                 val response = api.getMenu()
-                Log.d("MenuItemRepositoryyyyy","item"+response.body()?.record?.menu?.hamburgers?.get(0)?.name)
                 val allItems: List<MenuItem> = buildList {
                     addAll(response.body()?.record?.menu?.hamburgers.orEmpty().map { it.copy(category = "hamburgers") })
                     addAll(response.body()?.record?.menu?.pasta.orEmpty().map { it.copy(category = "pasta") })
                     addAll(response.body()?.record?.menu?.drinks.orEmpty().map { it.copy(category = "drinks") })
                     addAll(response.body()?.record?.menu?.sandwiches.orEmpty().map { it.copy(category = "sandwiches") })
                 }
-                Log.d("MenuItemRepositoryyyyy","item"+allItems.count())
                 dao.clearAll()
                 dao.insertAll(allItems)
                 allItems
